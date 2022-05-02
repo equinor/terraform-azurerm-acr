db471a6c039176ee2e329369ad7efee6c95088d5
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-05-02 08:40:40 +0200

    refactor: shorten output names

--- outputs.tf
+++ outputs.tf
@@ -1,52 +1,52 @@
-output "container_registry_id" {
+output "registry_id" {
   description = "The ID of the Container Registry."
   value       = azurerm_container_registry.this.id
 }
 
-output "container_registry_name" {
+output "registry_name" {
   description = "The name of the Container Registry."
   value       = azurerm_container_registry.this.name
 }
 
-output "container_registry_login_server" {
-  description = "The URL that can be used to log into the Container Registry."
+output "registry_login_server" {
+  description = "The login server of the Container Registry."
   sensitive   = true
   value       = azurerm_container_registry.this.login_server
 }
 
-output "container_registry_admin_username" {
-  description = "The Username associated with the Container Registry Admin account."
+output "registry_admin_username" {
+  description = "The admin username of the Container Registry"
   sensitive   = true
   value       = azurerm_container_registry.this.admin_username
 }
 
-output "container_registry_admin_password" {
-  description = "The Password associated with the Container Registry Admin account."
+output "registry_admin_password" {
+  description = "The admin password of the Container Registry."
   sensitive   = true
   value       = azurerm_container_registry.this.admin_password
 }
 
-output "user_assigned_identity_name" {
-  description = "The name of the User Assigned Identity."
+output "identity_name" {
+  description = "The name of the Managed Identity."
   value       = azurerm_user_assigned_identity.this.name
 }
 
-output "user_assigned_identity_id" {
-  description = "The ID of the User Assigned Identity."
+output "identity_id" {
+  description = "The ID of the Managed Identity."
   value       = azurerm_user_assigned_identity.this.id
 }
 
-output "user_assigned_identity_principal_id" {
-  description = "Client ID associated with the user assigned identity."
+output "identity_principal_id" {
+  description = "The principal ID of the Managed Identity."
   value       = azurerm_user_assigned_identity.this.principal_id
 }
 
-output "user_assigned_identity_client_id" {
-  description = "Client ID associated with the user assigned identity."
+output "identity_client_id" {
+  description = "The client ID of the Managed Identity."
   value       = azurerm_user_assigned_identity.this.client_id
 }
 
-output "user_assigned_identity_tenant_id" {
-  description = "Tenant ID associated with the user assigned identity."
+output "identity_tenant_id" {
+  description = "The tenant ID of the Managed Identity."
   value       = azurerm_user_assigned_identity.this.tenant_id
 }
