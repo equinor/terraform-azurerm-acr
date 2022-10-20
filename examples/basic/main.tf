fac5b13eda0abca9977d4289393b323009088993
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2022-10-20 14:34:10 +0200

    chore: update basic example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -11,11 +11,20 @@ resource "azurerm_resource_group" "this" {
   location = var.location
 }
 
+module "log_analytics" {
+  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.1.1"
+
+  workspace_name      = "log-${random_id.this.hex}"
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
+
 module "acr" {
   # source = "github.com/equinor/terraform-azurerm-acr"
   source = "../.."
 
-  registry_name       = "cr${random_id.this.hex}"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  registry_name              = "cr${random_id.this.hex}"
+  location                   = azurerm_resource_group.this.location
+  resource_group_name        = azurerm_resource_group.this.name
+  log_analytics_workspace_id = module.log_analytics.workspace_id
 }
