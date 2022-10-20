59b21d1b09bec74e88a11275410f2517934040ec
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-10-20 09:53:47 +0200

    chore: update example

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -7,7 +7,7 @@ resource "random_id" "this" {
 }
 
 resource "azurerm_resource_group" "this" {
-  name     = "rg-${random_id.this}"
+  name     = "rg-${random_id.this.hex}"
   location = var.location
 }
 
@@ -15,7 +15,7 @@ module "acr" {
   # source = "github.com/equinor/terraform-azurerm-acr"
   source = "../.."
 
-  registry_name       = "cr${random_id.this}"
+  registry_name       = "cr${random_id.this.hex}"
   location            = azurerm_resource_group.this.location
   resource_group_name = azurerm_resource_group.this.name
 }
