6b5547c7f76fc406da37f43ecabc9fca9122fe74
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2023-02-15 07:11:15 +0100

    feat!: disable acr admin account (#37)
    Disable Azure Container Registry admin account by default to follow Azure recommendation.
    
    BREAKING CHANGE: admin account disabled by default. To upgrade, set `admin_enabled` to `true`.

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_container_registry" "this" {
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku
-  admin_enabled       = true
+  admin_enabled       = var.admin_enabled
 
   dynamic "georeplications" {
     for_each = var.georeplications
