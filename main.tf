ee7e118072cd681c06786cf8f1af945a5e6ccdb3
Helena Solberg Kallekleiv <110530547+helenakallekleiv@users.noreply.github.com>
2023-04-20 12:29:01 +0200

    feat: set diagnostic setting enabled log categories

--- main.tf
+++ main.tf
@@ -39,12 +39,12 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
   log_analytics_workspace_id     = var.log_analytics_workspace_id
   log_analytics_destination_type = var.log_analytics_destination_type
 
-  enabled_log {
-    category = "ContainerRegistryLoginEvents"
-  }
+  dynamic "enabled_log" {
+    for_each = toset(var.diagnostic_setting_enabled_log_categories)
 
-  enabled_log {
-    category = "ContainerRegistryRepositoryEvents"
+    content {
+      category = enabled_log.value
+    }
   }
 
   metric {
