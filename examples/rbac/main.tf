d38e8de469ca1b0942be40aaa438944b8db1c13a
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-07 14:34:53 +0100

    docs: development and testing

--- examples/rbac/main.tf
+++ examples/rbac/main.tf
@@ -6,17 +6,12 @@ resource "random_id" "this" {
   byte_length = 8
 }
 
-resource "azurerm_resource_group" "this" {
-  name     = "rg-${random_id.this.hex}"
-  location = var.location
-}
-
 module "log_analytics" {
-  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v1.5.0"
+  source = "github.com/equinor/terraform-azurerm-log-analytics?ref=v2.1.1"
 
   workspace_name      = "log-${random_id.this.hex}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = var.resource_group_name
+  location            = var.location
 }
 
 module "acr" {
@@ -24,15 +19,15 @@ module "acr" {
   source = "../.."
 
   registry_name              = "cr${random_id.this.hex}"
-  location                   = azurerm_resource_group.this.location
-  resource_group_name        = azurerm_resource_group.this.name
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
   log_analytics_workspace_id = module.log_analytics.workspace_id
 }
 
 resource "azurerm_user_assigned_identity" "this" {
   name                = "id-acr-user-${random_id.this.hex}"
+  resource_group_name = var.resource_group_name
   location            = var.location
-  resource_group_name = azurerm_resource_group.this.name
 }
 
 resource "azurerm_role_assignment" "acr_pull" {
