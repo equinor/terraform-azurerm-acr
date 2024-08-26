91c39dfde9edc4069ce50de0a5b753620e7cdf23
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2024-08-26 12:38:30 +0200

    chore: update output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,30 +1,30 @@
 output "registry_id" {
-  description = "The ID of the Container Registry."
+  description = "The ID of this Container Registry."
   value       = azurerm_container_registry.this.id
 }
 
 output "registry_name" {
-  description = "The name of the Container Registry."
+  description = "The name of this Container Registry."
   value       = azurerm_container_registry.this.name
 }
 
 output "registry_login_server" {
-  description = "The login server of the Container Registry."
+  description = "The login server of this Container Registry."
   value       = azurerm_container_registry.this.login_server
 }
 
 output "registry_admin_username" {
-  description = "The admin username of the Container Registry."
+  description = "The admin username of this Container Registry."
   value       = azurerm_container_registry.this.admin_username
 }
 
 output "registry_admin_password" {
-  description = "The admin username of the Container Registry."
+  description = "The admin username of this Container Registry."
   sensitive   = true
   value       = azurerm_container_registry.this.admin_password
 }
 
 output "sku" {
-  description = "The SKU tier for the Container Registry."
+  description = "The SKU tier of this Container Registry."
   value       = azurerm_container_registry.this.sku
 }
