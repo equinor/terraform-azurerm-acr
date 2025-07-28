9076adffd0d6a3bffe6292dc02f84423c1ea7794
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2025-07-28 09:43:48 +0200

    fix: argument `metric` for resource `azurerm_monitor_diagnostic_setting` has been deprecated (#80)
    - Replace argument `metric` with `enabled_metric`.
    - Bump minimum allowed Azure provider version. Version 4.31.0 is required to use the `enabled_metric` argument.

--- main.tf
+++ main.tf
@@ -4,8 +4,6 @@ locals {
   georeplications = {
     for georeplication in var.georeplications : georeplication.location => georeplication
   }
-
-  diagnostic_setting_metric_categories = ["AllMetrics"]
 }
 
 resource "azurerm_container_registry" "this" {
@@ -70,13 +68,11 @@ resource "azurerm_monitor_diagnostic_setting" "this" {
     }
   }
 
-  dynamic "metric" {
-    for_each = toset(concat(local.diagnostic_setting_metric_categories, var.diagnostic_setting_enabled_metric_categories))
+  dynamic "enabled_metric" {
+    for_each = toset(var.diagnostic_setting_enabled_metric_categories)
 
     content {
-      # Azure expects explicit configuration of both enabled and disabled metric categories.
       category = metric.value
-      enabled  = contains(var.diagnostic_setting_enabled_metric_categories, metric.value)
     }
   }
 }
